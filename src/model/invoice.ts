109371b3012cf6c266df83563354338f041acc24
Ichiroku <danieliyo65@gmail.com>
2022-05-12 12:58:32 +0200

    Changed paths wording for a better understanding

--- src/model/invoice.ts
+++ src/model/invoice.ts
@@ -5,8 +5,8 @@ const invoiceSchema = new Schema({
     date: { type: Date, default: Date.now },
     total_price: { type: Double, required: true },
     user: { type: Schema.Types.ObjectId, ref: 'user' },
-    combinations: [{
-        product: { type: Schema.Types.ObjectId, ref: 'combination' },
+    products: [{
+        combination: { type: Schema.Types.ObjectId, ref: 'combination' },
         amount: { type: Number, required: true },
         price: { type: Double, required: true }
     }]
