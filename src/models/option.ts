a018888f8221155110a134e6f836ef306508fc69
Ichiroku <danieliyo65@gmail.com>
2022-06-08 19:13:56 +0200

    Removed order property from option

--- src/models/option.ts
+++ src/models/option.ts
@@ -2,8 +2,7 @@ import { Schema, model } from 'mongoose';
 
 // Lots of nullable values because the default values will come from the product
 export const optionSchema = new Schema({
-    name: { type: String, required: true, unique: true },
-    order: { type: Number, required: true, default: 0 }
+    name: { type: String, required: true, unique: true }
 }, { timestamps: true });
 
 export const Option = model('option', optionSchema);
